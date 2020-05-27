27b102e07060f169feb0e1a25c416ad71f061ea6
Anton Barkovsky <anton@swarmer.me>
2020-05-27 14:00:36 +0300

    Revert obsolete changes

--- rusoto/core/src/proto/json/payload.rs
+++ rusoto/core/src/proto/json/payload.rs
@@ -6,34 +6,28 @@ use serde_json::from_slice;
 use super::super::super::request::BufferedHttpResponse;
 use super::super::super::RusotoError;
 
-static EMPTY_DICT: Bytes = Bytes::from_static(b"{}");
-
-pub struct ResponsePayload<'a> {
-    body: &'a Bytes,
+pub struct ResponsePayload {
+    body: Bytes,
 }
 
-impl<'a> ResponsePayload<'a> {
-    pub fn new(res: &'a BufferedHttpResponse) -> Self {
-        debug!("Response status: {}", res.status);
-        Self::from_body(&res.body)
-    }
-
-    pub fn from_body(body: &'a Bytes) -> Self {
-        let mut processed_body = body;
+impl ResponsePayload {
+    pub fn new(res: &BufferedHttpResponse) -> Self {
+        let mut body = res.body.clone();
 
         // `serde-json` serializes field-less structs as "null", but AWS returns
         // "{}" for a field-less response, so we must check for this result
         // and convert it if necessary.
-        if processed_body.is_empty() || processed_body.as_ref() == b"null" {
-            processed_body = &EMPTY_DICT;
+        if body.is_empty() || body.as_ref() == b"null" {
+            body = Bytes::from_static(b"{}");
         }
 
-        debug!("Response body: {:?}", processed_body);
+        debug!("Response body: {:?}", body);
+        debug!("Response status: {}", res.status);
 
-        Self { body: processed_body }
+        Self { body }
     }
 
     pub fn deserialize<T: DeserializeOwned, E>(&self) -> Result<T, RusotoError<E>> {
-        Ok(from_slice(self.body)?)
+        Ok(from_slice(&self.body)?)
     }
 }
