a1448a2e79ab786cc1e2e4d61f640d1b9c3cd6ac
Anton Barkovsky <anton@swarmer.me>
2020-05-28 18:50:25 +0300

    Cargo fmt the new code

--- rusoto/core/src/event_stream.rs
+++ rusoto/core/src/event_stream.rs
@@ -8,7 +8,7 @@ use std::pin::Pin;
 use futures::task::{Context, Poll};
 use futures::Stream;
 use pin_project::pin_project;
-use serde::de::{Deserializer};
+use serde::de::Deserializer;
 
 use crate::error::RusotoError;
 use crate::request::HttpResponse;
@@ -17,7 +17,10 @@ use crate::stream::ByteStream;
 /// TODO
 pub trait DeserializeEvent: Sized {
     /// TODO
-    fn deserialize_event<'de, D: Deserializer<'de>>(event_type: &str, deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>;
+    fn deserialize_event<'de, D: Deserializer<'de>>(
+        event_type: &str,
+        deserializer: D,
+    ) -> Result<Self, <D as Deserializer<'de>>::Error>;
 }
 
 /// Event Stream.
@@ -67,7 +70,10 @@ impl<T: DeserializeEvent> futures::stream::Stream for EventStream<T> {
                 Ok(byte_chunk) => {
                     // TODO
                     println!("Got bytes: {:?}", byte_chunk);
-                    if byte_chunk.windows(16).any(move |sub_slice| sub_slice == b"initial-response") {
+                    if byte_chunk
+                        .windows(16)
+                        .any(move |sub_slice| sub_slice == b"initial-response")
+                    {
                         return Poll::Pending;
                     }
 
@@ -76,9 +82,9 @@ impl<T: DeserializeEvent> futures::stream::Stream for EventStream<T> {
                     let mut json_end = None;
                     for (i, c) in (&byte_chunk[json_start..]).iter().enumerate() {
                         match c {
-                            b'{' => { obj_depth += 1 },
-                            b'}' => { obj_depth -= 1 },
-                            _ => {},
+                            b'{' => obj_depth += 1,
+                            b'}' => obj_depth -= 1,
+                            _ => {}
                         }
 
                         if obj_depth == 0 {
@@ -92,14 +98,15 @@ impl<T: DeserializeEvent> futures::stream::Stream for EventStream<T> {
 
                     // TODO
                     let mut deserializer = serde_json::Deserializer::from_slice(&json_bytes);
-                    let parsed_event = T::deserialize_event("SubscribeToShardEvent", &mut deserializer);
+                    let parsed_event =
+                        T::deserialize_event("SubscribeToShardEvent", &mut deserializer);
                     // let parsed_event = proto::json::ResponsePayload::from_body(&Bytes::from(extended_json))
                     //     .deserialize::<T, _>();
                     Poll::Ready(Some(parsed_event.map_err(RusotoError::from)))
-                },
+                }
                 Err(e) => Poll::Ready(Some(Err(RusotoError::from(e)))),
             },
-            None => Poll::Ready(None)
+            None => Poll::Ready(None),
         }
     }
 }
